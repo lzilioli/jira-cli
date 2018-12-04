75345743b6b38a5fc3146b2c6099f50420c0d672
Luke Zilioli <luke@prism.fm>
2018-12-04 12:21:13 -0600

    color the output

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 const program = require('commander');
 const pkg = require('./package.json');
+const colors = require('colors');
 
 // JIRA API DOCS => https://jira-node.github.io
 const {jira, getIssueUrl} = require('./get-jira')();
-
 
 program
 .version(pkg.version)
@@ -13,10 +13,10 @@ program
 	// TODO make prefix optional
 	jira.findIssue(issueNumber)
 	.then(function(issue) {
-		console.log('Summary : ' + issue.fields.summary);
-		console.log('Status  : ' + issue.fields.status.name);
-		console.log('Assignee: ' + issue.fields.assignee.displayName);
-		console.log('URL     : ' + getIssueUrl(issueNumber));
+		logField('Summary  : ', colors.blue(issue.fields.summary));
+		logField('Status   : ', colorStatus(issue.fields.status.name));
+		logField('Assignee : ', colors.grey(issue.fields.assignee.displayName));
+		logField('URL      : ', colors.grey(getIssueUrl(issueNumber)));
 	})
 	.catch(function(err) {
 		console.error(err.message);
@@ -27,4 +27,27 @@ program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
+}
+
+//////////
+// Helpers
+//////////
+
+function getStatusColor(status){
+	const statusMap = {
+		'Pending Deployment': 'green',
+		'Done': 'green',
+		'Open': 'grey',
+		'Priority Pending': 'grey'
+	};
+	const color = statusMap[status] || 'blue';
+	return color;
+}
+
+function colorStatus(status){
+	return colors[getStatusColor(status)](status);
+}
+
+function logField(title, data){
+	console.log(colors.grey(title), data);
 }
